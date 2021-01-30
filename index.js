ec64d753452b797966d11c1b8a582590e009d979
vaibhav <vgohil891@rku.ac.in>
2021-01-30 20:35:01 +0530

    addad port,start script,connection string

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 var bodyParser = require('body-parser')
-mongoose.connect("mongodb://localhost:27017/tech",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://vaibhav:Vaibhav@2000@cluster0.j4iza.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(express.json());
         app.use("/api",route);
-        app.listen(3000,()=>{
+        app.listen(process.env.PORT || 3000,()=>{
             console.log("server started..!!");
         })
     }
