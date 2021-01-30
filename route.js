ef14e8a82025e0fdf5e82f16721a259a5942676e
vaibhav <vgohil891@rku.ac.in>
2021-01-30 20:16:53 +0530

    feaching completed

---
+++ route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const Book =require('./model/book');
+const router = express.Router('./route');
+
+router.get("/books",async(req,res)=>{
+
+    const book = await Book.find();
+    res.send(book);
+});
+module.exports = router;
