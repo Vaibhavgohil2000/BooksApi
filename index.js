a91826b3b4b1974a07f46d4ff897a8581a194be6
vaibhav <vgohil891@rku.ac.in>
2021-01-30 21:00:13 +0530

    user update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 var bodyParser = require('body-parser')
-mongoose.connect("mongodb+srv://vaibhav:Vaibhav@2000@cluster0.j4iza.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://Vaibhav:2000@cluster0.j4iza.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
         app.use(bodyParser.urlencoded({ extended: true }));
