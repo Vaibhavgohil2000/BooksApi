e4b3fcc7b83aaaec8de0a5a532dedda87a7f5d59
Vaibhavgohil2000 <68892759+Vaibhavgohil2000@users.noreply.github.com>
2021-02-02 11:43:41 +0530

    Update index.js
    testing with static path

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 var bodyParser = require('body-parser')
-mongoose.connect("mongodb+srv://Vaibhav:2000@cluster0.j4iza.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
-    ()=>{
-        const app = express();
-       app.get("/",(req,res)=>{
-       res.send("hello");
-       });
-        //app.use("/api",route);
-        app.listen(process.env.PORT || 3000,()=>{
-            console.log("server started..!!");
-        })
-    }
-);
+
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("adadas");
+});
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("server started");
+})
