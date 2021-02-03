692d073a71eb2f09cbef63eaf9b25b1b92bcf2cc
Vaibhavgohil2000 <68892759+Vaibhavgohil2000@users.noreply.github.com>
2021-02-03 17:58:59 +0530

    Update file

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 var bodyParser = require('body-parser')
-
-const app = express();
-app.get("/",(req,res)=>{
-    res.send("adadas");
-});
-
-app.listen(process.env.PORT || 3000,()=>{
-    console.log("server started");
-})
+mongoose.connect("mongodb+srv://Vaibhav:2000@cluster0.j4iza.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+    ()=>{
+        const app = express();
+       
+        app.use("/api",route);
+        app.listen(process.env.PORT || 3000,()=>{
+            console.log("server started..!!");
+        })
+    }
+);
